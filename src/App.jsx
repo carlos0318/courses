e235e1632bf177bde1a3d05f9cb5d5b38edd8809
Carlos Daniel Ortega Hernandez <carlos.ortega.ast0318@gmail.com>
2022-09-07 12:47:39 -0500

    feat: obtain the data and add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import courses from "./data/courses.json";
+import ConnectionsSVG from "./components/ConnectionsSVG";
+import OrganizationSVG from "./components/OrganizationSVG";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [favorites, setFavorite] = useState([]);
+  const [noFavorites, setNoFavorite] = useState([]);
+  let navigate = useNavigate();
 
+  useEffect(() => {
+    const favorite = async () => {
+      const favorite = await courses.filter((course) => course.is_favorite);
+      const noFavorite = await courses.filter((course) => !course.is_favorite);
+
+      setFavorite([
+        favorite[0],
+        favorite[1],
+        favorite[2],
+        favorite[3],
+        favorite[4],
+      ]);
+      setNoFavorite([
+        noFavorite[0],
+        noFavorite[1],
+        noFavorite[2],
+        noFavorite[3],
+        noFavorite[4],
+      ]);
+    };
+
+    favorite();
+  }, []);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App__isFavorite">
+        <div className="container">
+          <div className="App__title">
+            <ConnectionsSVG />
+            <div className="App__desc">
+              <span>Training powered by</span>
+              <h2>MY CONNECTIONS</h2>
+            </div>
+          </div>
+          <button className="btn" onClick={() => navigate('/favorites')}>View more</button>
+        </div>
+        <div className="App__container">
+          {favorites?.map((favorite) => (
+            <Card
+              key={favorite.id}
+              img={favorite.image}
+              language={favorite.language}
+              country={favorite.country}
+              isFavorite={favorite.is_favorite}
+              company={favorite.company_name}
+              title={favorite.title}
+            />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="App__isNotFavorite">
+        <div className="container">
+          <div className="App__title">
+            <OrganizationSVG />
+            <div className="App__desc">
+              <span>Training powered by</span>
+              <h2>MY ORGANIZATION</h2>
+            </div>
+          </div>
+          <button className="btn" onClick={() => navigate('/nofavorites')}>View more</button>
+        </div>
+        <div className="App__container">
+          {noFavorites?.map((noFavorite) => (
+            <Card
+              key={noFavorite.id}
+              img={noFavorite.image}
+              language={noFavorite.language}
+              country={noFavorite.country}
+              isFavorite={noFavorite.is_favorite}
+              company={noFavorite.company_name}
+              title={noFavorite.title}
+            />
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
