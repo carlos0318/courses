e044a7af1b82d4e12a20c4381ef6a4260cc9f964
Carlos Daniel Ortega Hernandez <carlos.ortega.ast0318@gmail.com>
2022-09-07 12:50:47 -0500

    feat: add react router and add three pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Favorites from './components/Favorites';
+import NoFavorites from './components/NoFavorites';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/nofavorites" element={<NoFavorites />} />
+    </Routes>
+  </Router>
+);
